3c49dcfaa7d7688c8546aa57c440c836cf6d1cb3
Romjkez <meshkov.ra@ya.ru>
2021-05-11 14:14:03 +0300

    chore: minor refactor and changes

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,5 +1,8 @@
 import { DEFAULT_PAGE } from './dto/get-many.dto';
+import { SelectQueryBuilder } from 'typeorm';
 
 export function calculateQueryOffset(perPage: number, page: number): number {
   return perPage * (page - 1) || DEFAULT_PAGE - 1;
 }
+
+export const isExistsQuery = <T>(builder: SelectQueryBuilder<T>): string => `exists (${builder.getQuery()})`;
